80fdfa39c5ff1c10a00fb7a82372425fcadad1f1
Maksim Ablameika <ablameika@netcracker.com>
2018-11-12 14:26:23 +0300

    First commit

---
+++ src/main/java/CoreFunctionality.java
@@ -0,0 +1,12 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class CoreFunctionality implements Functionality {
+
+    private static final Logger log = LoggerFactory.getLogger(CoreFunctionality.class);
+
+    public void doSomething() {
+        log.info("Executed.");
+    }
+}
---
+++ src/main/java/Executor.java
@@ -0,0 +1,8 @@
+
+public class Executor {
+
+    public static void main(String[] args) {
+        Functionality f = new CoreFunctionality();
+        f.doSomething();
+    }
+}
