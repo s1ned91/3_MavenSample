315079a033f3ec5de0978b97fe5b78a01dd31349
Maksim Ablameika <ablameika@netcracker.com>
2018-11-15 20:06:46 +0300

    5 commit

--- src/main/java/CoreFunctionality.java
+++ src/main/java/CoreFunctionality.java
@@ -6,6 +6,8 @@ public class CoreFunctionality implements Functionality {
 
     private static final Logger log = LoggerFactory.getLogger(CoreFunctionality.class);
 
+
+
     public void  doSomething() {
         log.info("Executed.");
     }
